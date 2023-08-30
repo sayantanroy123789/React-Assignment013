5923ead830378d5a68f5da07e449d213b5f8e176
sayantan <sayantan.roy123789@gmail.com>
2023-08-30 20:16:48 +0530

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: " Name the Father of the Indian Constitution?",
+        options:[ " Dr. B. R. Ambedkar", "Jawaharlal Nehru", "Mahatma Gandhi", "Subhas Chandra Bose"],
+        answer: 1
+    },
+    {
+        question: "Who was the first Prime Minister of India?",
+        options:[" Dr. B. R. Ambedkar", "Jawaharlal Nehru", "Mahatma Gandhi", "Subhas Chandra Bose"],
+        answer: 2
+    },
+    {
+        question: "When Indian national Anthem was first sung?",
+        options: [" August 15, 1947, Independence of Indian", "1857 revolt", " December 27, 1911 Calcutta", " 1919"],
+        answer: 3
+    },
+    {
+        question: "Where did the formation of Azad Hind Fauj take Place?",
+        options: ["Thailand", "Singapore", "Russia", "Germany"],
+        answer:2
+    },
+    {
+        question: "Who penned the book 'Wings of Fire'?",
+        options: ["APJ Abdul Kalam", "Indira Gandhi", "Vallabhbhai Patel", "none of the above"],
+        answer:1
+    },
+];
